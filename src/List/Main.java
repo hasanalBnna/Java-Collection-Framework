184370b2282b5e0e3832d7619ff0370eb4bfb2ab
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-14 12:48:54 +0600

    first commit

---
+++ src/List/Main.java
@@ -0,0 +1,22 @@
+
+
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+    //list
+    List list = new ArrayList();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+    list.add(2, 25);
+    list.add("Banna");
+    System.out.println(list);
+    }
+    
+}
