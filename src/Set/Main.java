fe734a641fe371f014efd97d1146b2d7bfc80f34
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-15 20:40:14 +0600

    seventh commit

--- src/Set/Main.java
+++ src/Set/Main.java
@@ -2,6 +2,7 @@
 package Set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 
@@ -9,12 +10,22 @@ public class Main {
 
 
     public static void main(String[] args) {
+        //Hashset
         Set<Integer> set= new HashSet<>();
         set.add(10);
         set.add(5);
         set.add(16);
         set.add(5);
-        System.out.println(set);
+        System.out.println("HashSet= "+set);
+        
+        //LinkedHashSet
+        LinkedHashSet<Integer> lhset = new LinkedHashSet<>();
+        lhset.add(5);
+        lhset.add(2);
+        lhset.add(2);
+        lhset.add(10);
+        System.out.println("LinkedHashSet= "+lhset);
+        
     
     }
     
