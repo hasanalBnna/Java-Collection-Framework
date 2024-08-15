d57a7a8df1fe5331137592da6e9f3890208633cb
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-15 19:37:49 +0600

    sixth commit

---
+++ src/Set/Main.java
@@ -0,0 +1,21 @@
+
+package Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Set<Integer> set= new HashSet<>();
+        set.add(10);
+        set.add(5);
+        set.add(16);
+        set.add(5);
+        System.out.println(set);
+    
+    }
+    
+}
