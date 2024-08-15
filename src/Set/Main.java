c96892c45ae579dcbd70af4fe61f429941cfcbbd
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-15 20:51:20 +0600

    9th commit

--- src/Set/Main.java
+++ src/Set/Main.java
@@ -4,6 +4,7 @@ package Set;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 
 public class Main {
@@ -27,6 +28,17 @@ public class Main {
         System.out.println("LinkedHashSet= "+lhset);
         boolean bl = lhset.contains(2);// check 2 is present or not
         System.out.println("2 is present not= "+bl);
+        
+        //TreeSet
+        System.out.println("TreeSet = Ascending order");
+        TreeSet<Integer> tset = new TreeSet<>();
+        tset.add(20);
+        tset.add(5);
+        tset.add(10);
+        tset.add(12);
+        tset.add(6);
+        tset.add(6);
+        System.out.println("TreeSet = "+tset);
     
     }
     
